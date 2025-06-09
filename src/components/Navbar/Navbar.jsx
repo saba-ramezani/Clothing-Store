1cc7230ddf5933272dd046e68144a15a37691488
saba-ramezani <saba.rad.sr@gmail.com>
2025-06-09 17:06:42 +0330

    added order btn in navber

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { logo } from '../../assets'
 import { FaSearch } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+
 
 
 const Navbar = () => {
@@ -26,7 +28,10 @@ const Topbar = () => {
             <input type="text" className='text-gray-200 w-[200px] group-hover:w-[300px] transition-all duration-300 focus:outline-none' placeholder='search' />
             <FaSearch size={15} className='text-gray-400 group-hover:text-[#fea928] ml-2' />
           </div>
-          <div></div>
+          <button className='group flex justify-center items-center transition-all duration-300 bg-gradient-to-r from-[#fea928] to-[#ed8900] h-[35px] px-5 rounded-[20px] gap-2'>
+            <span className='text-white group-hover:block hidden transition-all duration-300 font-semibold text-[17px]'>Order</span>
+            <FaShoppingCart size={20} className='text-white' />
+          </button>
           <div></div>
         </div>
       </div>
