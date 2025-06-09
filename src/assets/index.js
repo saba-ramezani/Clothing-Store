e544423fa4f2718b082e1fe1d31747f0b40eb93c
saba-ramezani <saba.rad.sr@gmail.com>
2025-06-09 19:31:41 +0330

    added mode switch and order btn in navbar

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,3 @@
 export {default as logo} from './logo.png'
+export {default as lightModeBtn} from './website/light-mode-button.png'
+export {default as darkModeBtn} from './website/dark-mode-button.png'
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
-import { logo } from '../../assets'
+import React, { useState } from 'react'
+import { darkModeBtn, lightModeBtn, logo } from '../../assets'
 import { FaSearch } from "react-icons/fa";
 import { FaShoppingCart } from "react-icons/fa";
+import classNames from "classnames";
+
 
 
 
@@ -16,6 +18,7 @@ const Navbar = () => {
 
 
 const Topbar = () => {
+  const [theme, setTheme] = useState('light');
   return (
     <div className='bg-[#785424] h-[55px] flex items-center justify-center'>
       <div className='max-w-[70%] w-[70%] flex justify-between items-center'>
@@ -23,19 +26,21 @@ const Topbar = () => {
           <img src={logo} alt="" className='w-[40px] h-[35px]' />
           <span className='text-white font-bold text-3xl'>Shopsy</span>
         </a>
-        <div className='flex'>
+        <div className='flex gap-3'>
           <div className='group flex justify-center items-center bg-[#202c34] h-[35px] border-[1px] border-gray-400 px-3 rounded-[20px]'>
             <input type="text" className='text-gray-200 w-[200px] group-hover:w-[300px] transition-all duration-300 focus:outline-none' placeholder='search' />
             <FaSearch size={15} className='text-gray-400 group-hover:text-[#fea928] ml-2' />
           </div>
-          <button className='group flex justify-center items-center transition-all duration-300 bg-gradient-to-r from-[#fea928] to-[#ed8900] h-[35px] px-5 rounded-[20px] gap-2'>
+          <button className='group flex justify-center items-center transition-all duration-300 bg-gradient-to-r from-[#e79924] to-[#d47c01] h-[35px] px-5 rounded-[20px] gap-2'>
             <span className='text-white group-hover:block hidden transition-all duration-300 font-semibold text-[17px]'>Order</span>
             <FaShoppingCart size={20} className='text-white' />
           </button>
-          <div></div>
+          <div className='flex items-center'>
+            <img src={lightModeBtn} className={classNames('h-[25px] cursor-pointer transition-all duration-300', theme === "light" ? "block" : "hidden")} onClick={() => setTheme('dark')} />
+            <img src={darkModeBtn} className={classNames('h-[25px] cursor-pointer transition-all duration-300', theme === "dark" ? "block" : "hidden")} onClick={() => setTheme('light')} />
+          </div>
         </div>
       </div>
-      
     </div>
   )
 }
